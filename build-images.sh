11c4b9162e9a92f62cef1d6ebd0fc1ab6adbe797
Matteo Valentini <matteo.valentini@nethesis.it>
2023-11-21 22:39:15 +0100

    Upadate loki version to 2.9.2

--- build-images.sh
+++ build-images.sh
@@ -10,7 +10,7 @@ container=$(buildah from scratch)
 buildah add "${container}" imageroot /imageroot
 buildah add "${container}" ui /ui
 buildah config --entrypoint=/ \
-	--label="org.nethserver.images=docker.io/traefik:v2.4 docker.io/grafana/loki:2.2.1" \
+	--label="org.nethserver.images=docker.io/traefik:v2.4 docker.io/grafana/loki:2.9.2" \
 	--label="org.nethserver.tcp-ports-demand=1" \
     --label='org.nethserver.flags=core_module' \
 	"${container}"
